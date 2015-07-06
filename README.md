8a35b2abe16684a787aa9a5af1024a93bc829c63
filipposerio <filippo.serio@gmail.com>
2015-07-06 14:55:08 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# strutturedem
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/StruttureController.js
@@ -0,0 +1,11 @@
+/**
+ * StruttureController
+ *
+ * @description :: Server-side logic for managing struttures
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
